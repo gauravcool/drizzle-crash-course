114e4ede1f8357e2eb3439fe6f5b25ef5f999d88
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-06-15 12:51:25 +0200

    finalizing setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
     schema: "./src/drizzle/schema.ts",
     out: "./src/drizzle/migrations",
-    driver: "d1-http",
     dialect: "postgresql",
     dbCredentials: {
-        connectionString: process.env.DATABASE_URL as string
+        url: process.env.DATABASE_URL as string
     },
     verbose: true,
     strict: true
