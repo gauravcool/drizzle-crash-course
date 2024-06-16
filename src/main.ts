bb770b552be1df6f93d93c923d5d9e75ab016ded
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-06-16 11:16:00 +0200

    insert/create operation

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,20 @@ import { db } from "./drizzle/db";
 import { UserTable } from "./drizzle/schema";
 
 async function main() {
-    await db.delete(UserTable)
+    const user = await db.insert(UserTable).values([{
+        name: "Gaurav",
+        age: 32,
+        email: "test@test.com"
+    }, {name: "Kumar", age: 29, email: "kumar@test.com"}])
+    .returning({
+        id: UserTable.id,
+        name: UserTable.name
+    }).onConflictDoUpdate({
+        target: UserTable.email, 
+        set: {name: "Updated Name"}
+    })
+    console.log(user);
+    
 }
 
 main();
