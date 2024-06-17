47ac59c6f107fe98fbc87e340cc06e63f77eb14f
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-06-17 07:13:37 +0200

    sql style sheet

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm"
 import { PgTable, boolean, integer, pgEnum, pgTable, primaryKey, real, timestamp, unique, uniqueIndex, uuid, varchar } from "drizzle-orm/pg-core"
 
 export const UserRole = pgEnum("userRole", ["ADMIN", "BASIC"])
@@ -46,3 +47,52 @@ export const PostCategoryTable = pgTable("postCategory", {
         pk: primaryKey({columns: [table.postId, table.categoryId]})
     }
 })
+
+//RELATIONS
+export const UserTableRelations = relations(UserTable, ({one, many}) => {
+    return {
+        preferences: one(UserPreferencesTable),
+        posts: many(PostTable)
+    }
+});
+
+export const UserPreferencesTableRelations = relations(UserPreferencesTable, ({one}) => {
+    return {
+        user: one(UserTable, {
+            fields: [UserPreferencesTable.userId],
+            references: [UserTable.id]
+        }),
+    }
+})
+
+export const PostTableRelations = relations(PostTable, ({one, many}) => {
+    return {
+        author: one(UserTable, {
+            fields: [PostTable.authorId],
+            references: [UserTable.id]
+        }),
+        postCategories: many(PostCategoryTable)
+    }
+})
+
+export const CategoryTableRelations = relations(CategoryTable, ({many}) => {
+    return {
+        postCategories: many(PostCategoryTable),
+    }
+})
+
+export const PostCategoryTableRelations = relations(
+    PostCategoryTable,
+    ({one}) => {
+        return {
+            post: one(PostTable, {
+                fields: [PostCategoryTable.postId],
+                references: [PostTable.id]
+            }),
+            category: one(CategoryTable, {
+                fields: [PostCategoryTable.categoryId],
+                references: [CategoryTable.id],
+            })
+        }
+    }
+)
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,16 @@
 import "dotenv/config"
 import { db } from "./drizzle/db"; 
-import { UserTable } from "./drizzle/schema";
+import { UserPreferencesTable, UserTable } from "./drizzle/schema";
+import { asc, sql } from "drizzle-orm";
 
 async function main() {
-    const user = await db.insert(UserTable).values([{
-        name: "Gaurav",
-        age: 32,
-        email: "test@test.com"
-    }, {name: "Kumar", age: 29, email: "kumar@test.com"}])
-    .returning({
-        id: UserTable.id,
-        name: UserTable.name
-    }).onConflictDoUpdate({
-        target: UserTable.email, 
-        set: {name: "Updated Name"}
-    })
-    console.log(user);
-    
+    // db.insert(UserPreferencesTable).values({
+    //     emailUpdates: true,
+    //     userId: 
+    // })
+
+    const users = await db.select().from(UserTable)
+    console.log(users);
 }
 
 main();
